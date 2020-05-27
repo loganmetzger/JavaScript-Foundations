6360bcc8204a46ec979e9e72c739d0fcf18ae86f
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-05-26 22:16:29 -0400

    Sort of finished all MVP goals. Need to refine mortgage calc function to get right value

--- index.js
+++ index.js
@@ -102,6 +102,14 @@ For example, variableInterestRate(200000, 0.04, 30) should console.log:
 "{Name}, with an interest rate of 0.06, your monthly rate is $1199"
 */
 
+function variableInterestRate() {
+    for (interestRate = 0.02; interestRate < 0.06; interestRate + 0.005) {
+        let numerator = principal * (interestRate * (Math.pow((1 + interestRate), periods)));
+        let denominator = (Math.pow((1 + interestRate), periods)) - 1;
+        return numerator / denominator;
+    }
+}
+console.log(variableInterestRate);
 
 
 
